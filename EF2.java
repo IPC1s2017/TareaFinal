59aa699250694227be0029562275fd26baf121db
Otzoy97 <otzoy97@ghotmail.com>
2017-08-21 19:10:40 -0600

    Actualización de EF2

--- EF2.java
+++ EF2.java
@@ -1,14 +1,13 @@
-public class BubbleSort {
+public class EF2 {
     public static void main(String[] args) {
-        int arr[] = {5,1,6,2,4,3}; 
-        BubbleSort bs = new BubbleSort();
-        bs.sort(arr);
-        bs.print(arr);
+        int arr[] = {2, 3, 8, 109, 13, 4, 18, 10, 23, 18, 50, 11, 13, 2}; 
+        EF2 ef = new EF2();
+        ef.sort(arr);
+        ef.print(arr);
     }
     public void sort(int array []){
         for(int i = 0;i < (array.length) ; i++){
             for (int j = 1; j < (array.length-i);j++){
-                this.print(array);
                 if (array[j-1]>array[j]){
                     int temp = array [j];
                     array[j] = array [j-1];
@@ -19,9 +18,15 @@ public class BubbleSort {
 
     }
     public void print(int array []){
-        System.out.print("{ ");
-        for (int i:array)
-            System.out.print(i + " ");
+        System.out.print("{");
+        for (int i = 0; i < array.length ; i++){
+            System.out.print(array[i]); 
+            if (i!=(array.length-1)) {
+	        System.out.print(", ");
+            } else {
+                break;
+            }
+        }
         System.out.print("}");
         System.out.println("");
     }  
