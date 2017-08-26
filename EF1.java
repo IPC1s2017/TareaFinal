633016fc646b2efeb79bb64b53953afabceaf1c6
SCajchum <smcajchum@gmail.com>
2017-08-26 00:01:19 -0600

    Ejercicios 1 y 2 de Tarea Final

--- EF1.java
+++ EF1.java
@@ -1,15 +1,13 @@
-public class EF1{
-	
-	public static void main(String args[]){
+//Ejercicio de Recursividad Factorial
+
+public class EF1 {
+	static int factorial (int numero) {
+		if (numero == 0) return 1;
+		else 
+		return numero * factorial(numero-1);
+	}
+	public static void main(String args[]) {
 		int n = Integer.parseInt(args[0]);
-		System.out.println(Factorial(n));
-	}	
-
-	public static int Factorial(int n){
-		if(){//Escribir condición de salida 
-			return 1;  
-		}else{//Escribir el retorno para la recursividad
-
-		}
+		System.out.println(factorial(n));	
 	}
 }
--- EF2.java
+++ EF2.java
@@ -1,9 +1,27 @@
-public class EF2{
-	
-	public static void main(String args[]){
-		
-		
-	}	
+//Ejercicio de BubbleSort
 
-	
+public class EF2{
+	public static void main(String args[]){
+		int array [] = {2,3,8,109,13,4,18,10,23,18,50,11,13,2};
+		EF2 bs = new EF2();
+		bs.Sort(array);
+		bs.print(array);
+	}
+	public void Sort (int array[]){
+		for(int i=0; i<array.length; i++){
+			for (int j =1; j<array.length-i; j++){
+				if (array[j-1]>array[j]){
+					int temp = array[j];
+					array[j]=array[j-1];
+					array[j-1]= temp;
+				}
+				this.print(array);
+			}
+		}
+	}
+	public void print (int array []){
+		for (int i=0; i<array.length; i++)
+		System.out.println(array[i]);
+		System.out.println("\n");
+	}
 }
