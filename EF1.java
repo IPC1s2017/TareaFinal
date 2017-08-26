5cc76c3920bb5f5c05dee6870d061ba4c8d27ad8
pedromendez2198 <30989996+pedromendez2198@users.noreply.github.com>
2017-08-26 13:39:31 -0600

    EF1 y EF2 201602946

--- EF1.java
+++ EF1.java
@@ -1,15 +1,31 @@
-public class EF1{
-	
-	public static void main(String args[]){
-		int n = Integer.parseInt(args[0]);
-		System.out.println(Factorial(n));
-	}	
+package ef1;
+/**
+ *
+ * @author lenovo
+ */
+public class EF1 {
 
-	public static int Factorial(int n){
-		if(){//Escribir condición de salida 
-			return 1;  
-		}else{//Escribir el retorno para la recursividad
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Entra como argumento en args[0]=5 args[1]=4 args[2]=3 args[3]=2 args[4]=8");
+        int n = Integer.parseInt(args[0]);
+        System.out.println("Sale factorial de args[0]: ");
+        System.out.println(Factorial(n));
+    }
 
-		}
-	}
+    private static int Factorial(int n) {
+         if (n==0 || n==1)
+            return 1;
+        else {
+            return (Factorial(n-1)*n);
+           
+        }
+        
+    }
+    
+    
 }
--- EF2.java
+++ EF2.java
@@ -1,9 +1,48 @@
-public class EF2{
-	
-	public static void main(String args[]){
-		
-		
-	}	
+package ef2;
 
-	
+/**
+ *
+ * @author lenovo
+ */
+public class EF2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int array[]= {2,3,8,109,13,4,18,10,23,18,50,11,13,2};
+        EF2 ps= new EF2();
+         ps.sort(array);
+        ps.print(array);
+        
+     
+    }
+    
+    public void sort(int array[]){
+        for (int i=0; i<array.length; i++){
+            
+            for (int j=1; j<array.length -i; j++){
+                if (array[j-1]>array[j]){
+                    int temporal=array[j];
+                    array[j]=array[j-1];
+                    array[j-1]=temporal;
+
+                }
+            }
+            
+        }
+    }
+    
+    public void print(int array[]){
+       //esto es un for each
+       System.out.print("{");
+       for (int i=0;i<array.length;i++){
+            System.out.print(array[i]);
+            if(i<array.length-1){
+                System.out.print(",");
+            }
+        }
+       System.out.println("}");
+    }
+    
 }
