d454391470cf960faf6763225d56411e49ed8d5b
MarisolRodriguez <30453708+MarisolRodriguez@users.noreply.github.com>
2017-08-26 14:28:33 -0600

    Add files via upload
    26/08/2017

--- EF1.java
+++ EF1.java
@@ -1,15 +1,26 @@
-public class EF1{
-	
-	public static void main(String args[]){
-		int n = Integer.parseInt(args[0]);
-		System.out.println(Factorial(n));
-	}	
+public class EF1 {
 
-	public static int Factorial(int n){
-		if(){//Escribir condición de salida 
-			return 1;  
-		}else{//Escribir el retorno para la recursividad
-
-		}
-	}
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el número del cual desea saber el factorial");
+        int numero = teclado.nextInt();
+        Factorial(numero);
+              
+    }
+    
+    public static int Factorial(int numero){
+        int r;
+        if(numero==0 || numero==1){
+            r = 1;
+        }else{
+            r = numero*Factorial(numero-1); 
+            
+        }
+        
+        System.out.println("El factorial de "+numero+"es: "+r);
+        return r;
+    }
+    
 }
--- EF2.java
+++ EF2.java
@@ -1,9 +1,36 @@
 public class EF2{
-	
-	public static void main(String args[]){
-		
-		
-	}	
 
-	
+    
+    public static void main(String[] args) {
+        
+        int array[] ={2, 3, 8, 109, 13, 4, 18, 10, 23, 18, 50, 11, 13, 2};
+        BubbleSort bs= new BubbleSort();
+        bs.sort(array);
+        bs.print(array);
+        
+    }
+    
+    public void sort(int array[]){
+        
+        for(int i=0; i<=array.length; i++){
+            for(int j=1; j<array.length; j++){
+                if(array[j-1]>array[j]){
+                    int temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1]=temp;
+                }
+            }
+        }
+            
+    }
+    
+    public void print(int array[]){
+        System.out.println("{");
+        for(int i : array){ //Esto se conoce como foreach
+            System.out.print(i+",");
+            System.out.println("");
+        }
+        System.out.println("}");
+    }
+   
 }
