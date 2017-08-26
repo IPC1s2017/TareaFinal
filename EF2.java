4da24e43c48aa421fc3f64f8dcddbd520e9aed58
JohanChan <30308066+JohanChan@users.noreply.github.com>
2017-08-25 19:52:24 -0600

    Ejercicio para 26/8/2017

--- EF2.java
+++ EF2.java
@@ -1,9 +1,32 @@
-public class EF2{
-	
-	public static void main(String args[]){
-		
-		
-	}	
+public class EF2 {
 
-	
+  
+    public static void main(String[] args) {
+        int vector[] = {2, 3, 8, 109, 13, 4, 18, 10, 23, 18, 50, 11, 13, 2};
+	Ordenamiento(vector);
+        Mostrar(vector);
+		
+    }
+    public static void Ordenamiento(int vector[]){
+        for(int i=0; i<vector.length;i++){
+            for(int j=0;j<vector.length-1;j++){
+                if(vector[j]>vector[j+1]){
+                    int aux = vector[j];
+                    vector[j] = vector[j+1];
+                    vector[j+1] = aux;
+                
+                }
+            }
+        
+        }
+    
+    }
+    public static void Mostrar(int vector[]){
+    for(int i=0;i<vector.length;i++){
+        System.out.print(+vector[i]+",");
+    
+    }
+        System.out.println(" ");
+    }
+    
 }
