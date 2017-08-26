c5af1c546e3d833361e89d96bbbbea970aa5c6df
deathstroke021 <thedeathstroke021@gmail.com>
2017-08-26 13:16:55 -0600

    Tarea Final

--- EF1.java
+++ EF1.java
@@ -1,15 +1,13 @@
 public class EF1{
-    
-    public static void main(String args[]){
-        int numero = Integer.parseInt(args[0]);
-        System.out.println(Factorial(numero));
-    }   
+	public static void main(String args[]){
+		int numero = Integer.parseInt(args[0]);
+		System.out.println(Factorial(numero));
+		}
+		public static int Factorial(int numero){
+			if(numero==1 || numero==0){
+				return 1;
+			}else{return numero*Factorial(numero-1);
 
-    public static int Factorial(int numero){
-        if(numero==1 || numero==0){
-            return 1;  
-        }else{return numero*Factorial(numero-1);
-
-        }
-    }
+		}
+	}
 }
