0a5ad9cab9b5f5448b974324e95e7f0e08833f72
rigorzs <rzrigozs@gmail.com>
2017-08-26 10:26:05 -0600

    Ejercicos EF1 Y EF2

--- EF1.java
+++ EF1.java
@@ -1,15 +1,15 @@
 public class EF1{
-	
-	public static void main(String args[]){
-		int n = Integer.parseInt(args[0]);
-		System.out.println(Factorial(n));
-	}	
-
-	public static int Factorial(int n){
-		if(){//Escribir condición de salida 
-			return 1;  
-		}else{//Escribir el retorno para la recursividad
-
+	public static void main(String[]args){
+		//int n=Integer.parseInt(args[]);
+		int n= Integer.parseInt(args[0]);
+		EF1 bs = new EF1();
+        System.out.println(bs.factorial(n));	
+	}
+	public static int factorial(int n){
+		if(n==0){
+			return 1;
+		}else{
+			return n*(factorial(n-1));
 		}
 	}
 }
--- EF2.java
+++ EF2.java
@@ -1,9 +1,37 @@
 public class EF2{
-	
-	public static void main(String args[]){
-		
-		
-	}	
-
-	
+	public static void main(String[]args){
+		//int n=Integer.parseInt(args[]);
+		int array[]={2,3,8,109,13,4,18,10,23,18,50,11,13,2};
+        EF2 bs= new EF2();
+        bs.sort(array);
+        bs.print(array);	
+	}
+	    public void sort(int array[]){
+        
+        for(int i=0;i< array.length ;i++){
+            for(int j=1;j<array.length-i;j++){
+                if(array[j-1]>array[j]){
+                    int temporal=array[j];
+                    array[j]=array[j-1];
+                    array[j-1]=temporal;
+                }
+            }
+        }
+    }
+	public void print(int array[]){
+        //declaracion foresach=es usado para recorrere arreglos
+          System.out.print("{");
+          int j=array.length;
+        for(int i : array){
+        	if(i<=j+50){
+        	System.out.print(i+","); 	
+        	}else{
+        		System.out.print(i);
+        	}	
+          	  
+        }
+        System.out.print("}");
+            System.out.println();
+        //fin declaracion foreach
+    }
 }
