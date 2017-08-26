d5f76c20e232294e82260d3f64b0e0fac7a37ac8
Adam547 <navas_garcia@hotmail.com>
2017-08-26 00:22:26 -0600

    EF1 y EF2

--- EF1.java
+++ EF1.java
@@ -1,15 +1,27 @@
-public class EF1{
-	
-	public static void main(String args[]){
-		int n = Integer.parseInt(args[0]);
-		System.out.println(Factorial(n));
-	}	
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ef1;
+import java.util.Scanner;
 
-	public static int Factorial(int n){
-		if(){//Escribir condición de salida 
-			return 1;  
-		}else{//Escribir el retorno para la recursividad
-
-		}
-	}
+/**
+ *
+ * @author Adam Navas
+ */
+public class EF1 {
+    public static int Num;
+    public static void main(String[] args) {
+      int N= Integer.parseInt(args[0]);
+      System.out.println(Fact(N));
+    }
+    public static int Fact(int N){
+        if (Num == 0){
+            return 1;
+        }else{
+            return Num * Fact(N-1);
+        }
+        
+    }
 }
--- EF2.java
+++ EF2.java
@@ -1,9 +1,44 @@
-public class EF2{
-	
-	public static void main(String args[]){
-		
-		
-	}	
+package ef2;
 
-	
+public class EF2 {
+    int orden[] = new int[14];
+    static int cont = 0;
+
+    public static void main(String[] args) {
+    int array[] = {2, 3, 8, 109, 13, 4, 18, 10, 23, 18, 50, 11, 13, 2};
+        System.out.println("Entrada: ");
+        System.out.println("{2, 3, 8, 109, 13, 4, 18, 10, 23, 18, 50, 11, 13, 2}");
+        EF2 bs= new EF2();
+        System.out.println();
+        System.out.println("Salida:");
+        bs.sort(array);
+    }
+    public void sort(int array[]){
+        for(int i=0; i < array.length-1;i++){
+            for(int j=1; j < array.length-1; j++){
+                if(array[j]>array[j+1]){
+                    int temp = array[j+1];
+                    array[j+1]= array[j];
+                    array[j]=temp;
+                }
+            }
+            for(int v=0; v < array.length;v++ ){
+                orden[v]= array[v];
+            } 
+        }
+            this.print(array);
+    }
+    public void print (int array[]){
+        System.out.print("{");
+    for(int i = 0; i < array.length;i++){
+        if (orden[i] == 109){
+            System.out.print(orden[i]);
+        }else{
+            System.out.print(orden[i]+", "); 
+        }        
+     }
+     System.out.print("}");
+     System.out.println();
+    }
 }
+
