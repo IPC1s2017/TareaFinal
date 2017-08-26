aa28997de299c5ca7ceaa9e985d02c2ac7dad4d3
PascoDominguez <pascodoms@hotmail.com>
2017-08-26 10:41:37 -0600

    Ejercicios para 26/8/2017

--- EF1.java
+++ EF1.java
@@ -1,15 +1,20 @@
-public class EF1{
-	
-	public static void main(String args[]){
-		int n = Integer.parseInt(args[0]);
-		System.out.println(Factorial(n));
-	}	
+public class EF1 {
 
-	public static int Factorial(int n){
-		if(){//Escribir condición de salida 
-			return 1;  
-		}else{//Escribir el retorno para la recursividad
+    public static void main(String args[]) {
+        int n = Integer.parseInt(args[0]);
+        EF1 ef = new EF1();
+        System.out.println(ef.factorial(n));
+    }
 
-		}
-	}
+    
+    
+    public static int factorial(int fac) {
+       
+        if (fac == 0) {
+            return 1;
+        } else {
+            return fac * (factorial(fac - 1));
+        }
+        
+    }
 }
