6f5ed50e7f50da002d180f1420fcfc12e6a38412
rcristales <robertcristales1993>
2017-09-06 22:09:55 -0600

    Tarea1 y 2 Ing. neftali

---
+++ Tarea2.java
@@ -0,0 +1,19 @@
+public class Tarea2{
+
+	public static void main(String[] args) {
+		for (int i = 0;i<=9;i++ ) {
+			for (int j=0;j<=9 ;j++ ) {
+				for (int k=0; k<=9; k++ ) {
+					for (int l=0;l<=9 ;l++ ) {
+						System.out.print(i);
+						System.out.print(j);
+						System.out.print(k);
+						System.out.print(l);
+						System.out.print(" ");
+					}
+				}
+			}
+		}
+		
+	}
+}
